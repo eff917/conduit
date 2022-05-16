68155810daffd661ba37d450f953b6ae5130f1a1
Felber Péter (Eff) <effpeter@gmail.com>
2022-05-16 14:42:32 +0200

    test selenium on local running container

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+URL = "http://localhost:1667/"
+WINDOW_SIZE = "1920,1080"
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+
+
 def test_dummy():
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    browser.get(URL)
     return True
