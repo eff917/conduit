bd885418e4b37a59c711f73364c5543dfcc02fd2
Felber Péter (Eff) <effpeter@gmail.com>
2022-05-16 16:51:51 +0200

    added pylint and black, updated workflow action versions

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,4 +1,7 @@
-import pytest
+"""
+Dummy module for pytest, needed to set up github actions
+"""
+import traceback
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,21 +12,32 @@ WINDOW_SIZE = "1920,1080"
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument(f"--window-size={WINDOW_SIZE}")
+
 
 def test_dummy():
+    """
+    Dummy test 1
+    """
     try:
-        browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        browser = webdriver.Chrome(
+            ChromeDriverManager().install(), options=chrome_options
+        )
         browser.get(URL)
-    except:
-        pass
+    except Exception as ex:  # pylint: disable=W0703
+        print(traceback.format_exc(ex))
     return True
+
 
 def test_dummy2():
+    """
+    Dummy test 2
+    """
     try:
-        browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        browser = webdriver.Chrome(
+            ChromeDriverManager().install(), options=chrome_options
+        )
         browser.get(URL)
-    except:
-        pass
+    except Exception as ex:  # pylint: disable=W0703
+        print(traceback.format_exc(ex))
     return True
-
