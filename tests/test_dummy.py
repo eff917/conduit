d4f733f6f7421008da6f84f5a85878081bb1ac90
Felber Péter (Eff) <effpeter@gmail.com>
2022-05-16 18:20:22 +0200

    take screenshot every 5 seconds

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,9 +1,11 @@
 """
 Dummy module for pytest, needed to set up github actions
 """
+from time import sleep
 import allure
 
 from allure_commons.types import AttachmentType
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -16,34 +18,19 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument(f"--window-size={WINDOW_SIZE}")
 
-
-def test_dummy():
+@pytest.mark.parametrize("time", range(0,61,5))
+def test_dummy(time):
     """
     Dummy test 1
     """
-    with allure.step('open conduit and take screenshot'):
+    with allure.step(f'open conduit and take screenshot after {time} seconds'):
         try:
             browser = webdriver.Chrome(
                 ChromeDriverManager().install(), options=chrome_options
             )
             browser.get(URL)
-            allure.attach(browser.get_screenshot_as_png, name="Dummy_test_1", attachment_type=AttachmentType.PNG)
+            allure.attach(browser.get_screenshot_as_png(), name=f"Dummy_test_{time}", attachment_type=AttachmentType.PNG)
         except Exception as ex:  # pylint: disable=W0703
             print(ex)
-    return True
-
-
-def test_dummy2():
-    """
-    Dummy test 2
-    """
-    with allure.step('open conduit again, and take screenshot'):
-        try:
-            browser = webdriver.Chrome(
-                ChromeDriverManager().install(), options=chrome_options
-            )
-            browser.get(URL)
-            allure.attach(browser.get_screenshot_as_png(), name="Dummy_test_2", attachment_type=AttachmentType.PNG)
-        except Exception as ex:  # pylint: disable=W0703
-            print(ex)
+    sleep(5)
     return True
