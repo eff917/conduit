d5b7beb91cad8d7651f85d6625c6b1deaa96de05
Felber Péter (Eff) <effpeter@gmail.com>
2022-05-16 17:21:02 +0200

    add screenshots to allure report

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,9 @@
 """
 Dummy module for pytest, needed to set up github actions
 """
+import allure
+
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,13 +21,15 @@ def test_dummy():
     """
     Dummy test 1
     """
-    try:
-        browser = webdriver.Chrome(
-            ChromeDriverManager().install(), options=chrome_options
-        )
-        browser.get(URL)
-    except Exception as ex:  # pylint: disable=W0703
-        print(ex)
+    with allure.step('open conduit and take screenshot'):
+        try:
+            browser = webdriver.Chrome(
+                ChromeDriverManager().install(), options=chrome_options
+            )
+            browser.get(URL)
+            allure.attach(browser.get_screenshot_as_png, name="Dummy_test_1", attachment_type=AttachmentType.PNG)
+        except Exception as ex:  # pylint: disable=W0703
+            print(ex)
     return True
 
 
@@ -32,11 +37,13 @@ def test_dummy2():
     """
     Dummy test 2
     """
-    try:
-        browser = webdriver.Chrome(
-            ChromeDriverManager().install(), options=chrome_options
-        )
-        browser.get(URL)
-    except Exception as ex:  # pylint: disable=W0703
-        print(ex)
+    with allure.step('open conduit again, and take screenshot'):
+        try:
+            browser = webdriver.Chrome(
+                ChromeDriverManager().install(), options=chrome_options
+            )
+            browser.get(URL)
+            allure.attach(browser.get_screenshot_as_png(), name="Dummy_test_2", attachment_type=AttachmentType.PNG)
+        except Exception as ex:  # pylint: disable=W0703
+            print(ex)
     return True
