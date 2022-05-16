b8c7d0ef67b6c0bbe498fe0d58a1815584ec918d
Felber Péter (Eff) <effpeter@gmail.com>
2022-05-16 23:30:53 +0200

    test wait for page load

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -3,13 +3,13 @@ Dummy module for pytest, needed to set up github actions
 """
 from time import sleep
 import allure
-
 from allure_commons.types import AttachmentType
 import pytest
+import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 URL = "http://localhost:1667/"
 WINDOW_SIZE = "1920,1080"
@@ -18,20 +18,50 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument(f"--window-size={WINDOW_SIZE}")
 
-# conduit start after ~30seconds
-@pytest.mark.parametrize("time", range(0,61,5))
+
+def test_if_main_page_loads():
+    """
+    Wait for docker container to load
+    """
+    with allure.step(f"open conduit and take screenshot after main page loaded"):
+        try:
+            driver = webdriver.Chrome(
+                ChromeDriverManager().install(), options=chrome_options
+            )
+            driver.get(URL)
+            wait = WebDriverWait(driver, 30, 5)
+            element = wait.until(driver.find_element_by_class_name("logo-font"))
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name=f"main_page",
+                attachment_type=AttachmentType.PNG,
+            )
+        except Exception as ex:  # pylint: disable=W0703
+            print(ex)
+        finally:
+            driver.quit()
+
+
+# conduit starts after 20-30seconds
+@pytest.mark.parametrize("time", range(0, 10, 5))
 def test_dummy(time):
     """
     Dummy test 1
     """
-    with allure.step(f'open conduit and take screenshot after {time} seconds'):
+    with allure.step(f"open conduit and take screenshot after {time} seconds"):
         try:
             browser = webdriver.Chrome(
                 ChromeDriverManager().install(), options=chrome_options
             )
             browser.get(URL)
-            allure.attach(browser.get_screenshot_as_png(), name=f"Dummy_test_{time}", attachment_type=AttachmentType.PNG)
+            allure.attach(
+                browser.get_screenshot_as_png(),
+                name=f"Dummy_test_{time}",
+                attachment_type=AttachmentType.PNG,
+            )
         except Exception as ex:  # pylint: disable=W0703
             print(ex)
+        finally:
+            browser.quit()
     sleep(5)
     return True
