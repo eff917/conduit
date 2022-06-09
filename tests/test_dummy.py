49162f89ca7bcd0df84d2253ca4994b9beed9baf
Felber Péter (Eff) <effpeter@gmail.com>
2022-06-09 23:44:35 +0200

    login test

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -26,6 +26,7 @@ def driver():
         driver = webdriver.Chrome(
             ChromeDriverManager().install(), options=chrome_options
         )
+
         yield driver
         driver.quit()
 
@@ -64,11 +65,30 @@ def test_login(driver):
     with allure.step(f"test login"):
         try:
             driver.get(URL)
+            login_link = driver.find_element_by_xpath('//a[@href="#/login"]')
+            login_link.click()
             allure.attach(
                 driver.get_screenshot_as_png(),
-                name=f"Dummy_test",
+                name=f"login_page",
                 attachment_type=AttachmentType.PNG,
             )
+            login_email_field = driver.find_element_by_xpath('//input[@placeholder="Email"]')
+            login_password_field = driver.find_element_by_xpath('//input[@placeholder="Password"]')
+            login_button = driver.find_element_by_xpath('//button[contains(text(), "Sign in")]')
+            login_email_field.sendkeys("user32@hotmail.com")
+            login_password_field.sendkeys("Userpass1")
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name=f"login_page_after_fill",
+                attachment_type=AttachmentType.PNG,
+            )
+            login_button.click()
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name=f"after_login",
+                attachment_type=AttachmentType.PNG,
+            )
+
         except Exception as ex:  # pylint: disable=W0703
             print(ex)
     sleep(5)
