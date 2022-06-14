63559537f1a8ac310925c8228406d95ed88b7009
Felber Péter (Eff) <effpeter@gmail.com>
2022-06-14 16:31:51 +0200

    placeholder files for all the needed tests

--- tests/test_02_cookie_policy.py
+++ tests/test_02_cookie_policy.py
@@ -8,12 +8,11 @@ def test_learn_more_link(driver):
     driver.delete_all_cookies()
     driver.get(URL)
     learn_more_link = driver.find_element_by_xpath('//a[@href="https://cookiesandyou.com/"]')
-    print(driver.title)
     learn_more_link.click()
     tab_titles = []
     for tab in driver.window_handles:
         driver.switch_to.window(tab)
-        take_screenshot(driver=driver, name=f'tab')
+        take_screenshot(driver=driver, name=f'{tab}')
         tab_titles.append(driver.title)
 
     assert 'What are cookies? | Cookies & You' in tab_titles
---
+++ tests/test_05_data_handling.py
@@ -0,0 +1,8 @@
+#test_05_list_data.py
+#test_06_multi_page_list.py
+#test_07_new_data.py
+#test_08_new_data_from_file.py
+#test_09_modify_data.py
+#test_10_delete_data.py
+#test_11_download_data.py
+
