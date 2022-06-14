cf2dd9758e95ea35354d6b68df43261cac386564
Felber Péter (Eff) <effpeter@gmail.com>
2022-06-14 12:40:49 +0200

    invalid login parametrized

--- tests/test_02_login.py
+++ tests/test_02_login.py
@@ -9,27 +9,31 @@ from .utils.allure_wrappers import take_screenshot
 
 URL = "http://localhost:1667/"
 
-def test_invalid_login(driver):
+@pytest.mark.parametrize(
+    "email, password, expected_message",
+    [
+        ("invalidmail", "Userpass1", "Email must be a valid email."),
+        ("user32@hotmail.com", "Userpass1", "Invalid user credentials."),
+    ]
+)
+def test_invalid_login(driver, email, password, expected_message):
     """
     Test invalid login
     """
     with allure.step(f"test login"):
-        try:
-            driver.get(URL)
-            login_link = driver.find_element_by_xpath('//a[@href="#/login"]')
-            login_link.click()
-            take_screenshot(driver, "login_page")
-            login_email_field = driver.find_element_by_xpath('//input[@placeholder="Email"]')
-            login_password_field = driver.find_element_by_xpath('//input[@placeholder="Password"]')
-            login_button = driver.find_element_by_xpath('//button[contains(text(), "Sign in")]')
-            login_email_field.send_keys("user32@hotmail.com")
-            login_password_field.send_keys("Userpass1")
-            take_screenshot(driver, "login_page_after_fill")
-            login_button.click()
-            take_screenshot(driver, "after_login")
-            # TODO create assert for error messages
-            # <div class="swal-text" style="">Email must be a valid email.</div>
-
-        except Exception as ex:  # pylint: disable=W0703
-            print(ex)
-    return True
+        driver.get(URL)
+        login_link = driver.find_element_by_xpath('//a[@href="#/login"]')
+        login_link.click()
+        take_screenshot(driver, "login_page")
+        login_email_field = driver.find_element_by_xpath('//input[@placeholder="Email"]')
+        login_password_field = driver.find_element_by_xpath('//input[@placeholder="Password"]')
+        login_button = driver.find_element_by_xpath('//button[contains(text(), "Sign in")]')
+        login_email_field.send_keys(email)
+        login_password_field.send_keys(password)
+        take_screenshot(driver, "login_page_after_fill")
+        login_button.click()
+        take_screenshot(driver, "after_login")
+        # TODO create assert for error messages
+        # <div class="swal-text" style="">Email must be a valid email.</div>
+        error_message = driver.find_element_by_xpath('//div[@class="swal-text"]')
+        assert error_message.text == expected_message
