54bf5163ed1411665baab0760d40bc6f6abba6ef
Felber Péter (Eff) <effpeter@gmail.com>
2022-06-14 14:46:23 +0200

    added elapsed time to main page load test screenshot

--- tests/test_01_main_page.py
+++ tests/test_01_main_page.py
@@ -3,6 +3,7 @@ Test main page with selenium
 """
 import allure
 import pytest
+import time
 from allure_commons.types import AttachmentType
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -17,6 +18,7 @@ def test_wait_for_main_page_load(driver):
     """
     Wait for docker container to load
     """
+    start_time = time.time()
     driver.get(URL)
     with allure.step(f"open conduit and take screenshot after main page loaded"):
         page_loaded_successfully = False
@@ -28,7 +30,8 @@ def test_wait_for_main_page_load(driver):
                 _ = WebDriverWait(driver, timeout=5, poll_frequency=1).until(
                     EC.presence_of_element_located((By.CLASS_NAME, "logo-font"))
                 )
-                take_screenshot(driver, f"Screenshot_after_wait_{i}")
+                elapsed_time = int(time.time() - start_time)
+                take_screenshot(driver, f"Screenshot_after_{elapsed_time}s")
                 # if the fluent wait doesn't raise an exception, break the cycle, else try again.
                 page_loaded_successfully = True
             except TimeoutException:
