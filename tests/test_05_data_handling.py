2aec96a68d2b5d1de7630eb1da9cfc6a8fcd0351
Felber Péter (Eff) <effpeter@gmail.com>
2022-06-29 16:29:46 +0200

    list data and multi page test

--- tests/test_05_data_handling.py
+++ tests/test_05_data_handling.py
@@ -19,9 +19,13 @@ def test_list_data(login_logout_driver):
     article_list = login_logout_driver.find_elements_by_xpath('//a[@class="preview-link"]/h1')
     assert len(article_list) != 0
 
-@pytest.mark.skip()
-def test_list_multi_page_data(driver):
-    assert True
+def test_list_multi_page_data(login_logout_driver):
+        page_nr_list = login_logout_driver.find_elements_by_xpath('//a[@class="page-link"]')
+        for page in page_nr_list:
+            page.click()
+            sleep(1)
+            actual_page = login_logout_driver.find_element_by_xpath('//li[@class="page-item active"]')
+            assert page.text == actual_page.text
 
 @pytest.mark.skip()
 def test_insert_data(driver):
