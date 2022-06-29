1447e47b66c86032fd9be234686dacfc7370c909
Felber Péter (Eff) <effpeter@gmail.com>
2022-06-29 19:44:09 +0200

    only download data test left

--- tests/test_05_data_handling.py
+++ tests/test_05_data_handling.py
@@ -11,6 +11,8 @@ from .utils.allure_wrappers import take_screenshot
 #test_10_download_data.py
 #test_11_delete_data.py
 
+URL = "http://localhost:1667/"
+
 def test_list_data(login_logout_driver):
     lorem_tag = login_logout_driver.find_element_by_xpath(
             '//div[@class="sidebar"]/div[@class="tag-list"]/a[text()="lorem"]')
@@ -24,14 +26,14 @@ def test_list_multi_page_data(login_logout_driver):
         page_nr_list = login_logout_driver.find_elements_by_xpath('//a[@class="page-link"]')
         for page in page_nr_list:
             page.click()
-            sleep(1)
+            sleep(2)
             actual_page = login_logout_driver.find_element_by_xpath('//li[@class="page-item active"]')
             assert page.text == actual_page.text
 
 def test_insert_data(login_logout_driver):
     new_article_link = login_logout_driver.find_element_by_xpath('//a[@href="#/editor"]')
     new_article_link.click()
-    sleep(1)
+    sleep(2)
     take_screenshot(login_logout_driver, "before_fill")
     title_field = login_logout_driver.find_element_by_xpath('//input[@placeholder="Article Title"]')
     about_field = login_logout_driver.find_element_by_xpath('//input[@placeholder="What\'s this article about?"]')
@@ -50,29 +52,82 @@ def test_insert_data(login_logout_driver):
     tags_field.send_keys("\n".join(tags_list))
     take_screenshot(login_logout_driver, "after_fill")
     publish_button.click()
-    sleep(1)
+    sleep(2)
     take_screenshot(login_logout_driver, "after_publish")
     actual_title = login_logout_driver.find_element_by_xpath('//h1')
     actual_tags_list = login_logout_driver.find_elements_by_xpath('//a[@class="tag-pill tag-default"]')
+
     for actual_tag, expected_tag in zip(actual_tags_list, tags_list):
         assert actual_tag.text == expected_tag
 
     assert actual_title.text == article_title
 
-
-
-@pytest.mark.skip()
-def test_bulk_insert_data_from_file(login_logout_driver):
-    assert True
-
-@pytest.mark.skip()
 def test_update_data(login_logout_driver):
-    assert True
+    login_logout_driver.get(URL + '#/articles/test-article')
+    sleep(2)
+    edit_button = login_logout_driver.find_element_by_xpath('//a[@href="#/editor/test-article"]')
+    edit_button.click()
+    sleep(2)
+    title_field = login_logout_driver.find_element_by_xpath('//input[@placeholder="Article Title"]')
+    publish_button = login_logout_driver.find_element_by_xpath('//button[@type="submit"]')
+
+    title_field.send_keys(" Modified")
+    publish_button.click()
+    sleep(2)
+    actual_title = login_logout_driver.find_element_by_xpath('//h1')
+    assert actual_title.text == "Test Article Modified"
+
 
 @pytest.mark.skip()
 def test_download_data(login_logout_driver):
     assert True
 
-@pytest.mark.skip()
 def test_delete_data(login_logout_driver):
-    assert True
+    login_logout_driver.get(URL + '#/articles/test-article')
+    sleep(2)
+    delete_button = login_logout_driver.find_element_by_xpath('//button[@class="btn btn-outline-danger btn-sm"]')
+    delete_button.click()
+    sleep(2)
+    title_list = login_logout_driver.find_elements_by_xpath('//h1')
+    titles = []
+    for a_title in title_list:
+        titles.append(a_title.text)
+    assert "Test Article Modified" not in titles
+
+def test_bulk_insert_data_from_file(login_logout_driver):
+    new_article_link = login_logout_driver.find_element_by_xpath('//a[@href="#/editor"]')
+    new_article_link.click()
+    sleep(1)
+    title_field = login_logout_driver.find_element_by_xpath('//input[@placeholder="Article Title"]')
+    about_field = login_logout_driver.find_element_by_xpath('//input[@placeholder="What\'s this article about?"]')
+    article_field = login_logout_driver.find_element_by_xpath('//textarea[@placeholder="Write your article (in markdown)"]')
+    tags_field = login_logout_driver.find_element_by_xpath('//input[@class="ti-new-tag-input ti-valid"]')
+    publish_button = login_logout_driver.find_element_by_xpath('//button[@type="submit"]')
+
+    with open('./tests/data/input.csv', 'r', encoding='utf8') as inputfile:
+        for line in inputfile:
+            # parse inputs
+            article_title, article_about, article_body, tags_list = line.split(',')
+            tags_list = tags_list.rstrip("\n").split(" ")
+            
+            new_article_link.click()
+            sleep(1)
+            take_screenshot(login_logout_driver, "before_fill")
+
+            title_field.send_keys(article_title)
+            about_field.send_keys(article_about)
+            article_field.send_keys(article_body)
+            tags_field.send_keys("\n".join(tags_list))
+            take_screenshot(login_logout_driver, "after_fill")
+            publish_button.click()
+            sleep(1)
+            take_screenshot(login_logout_driver, "after_publish")
+            actual_title = login_logout_driver.find_element_by_xpath('//h1')
+            actual_tags_list = login_logout_driver.find_elements_by_xpath('//a[@class="tag-pill tag-default"]')
+
+            for actual_tag, expected_tag in zip(actual_tags_list, tags_list):
+                assert actual_tag.text == expected_tag
+
+            assert actual_title.text == article_title
+
+
